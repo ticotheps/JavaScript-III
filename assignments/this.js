9d04f34cfd01ce1c4e438bad2bdbec42a63df5d0
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-01-23 16:42:57 -0500

    Completed code examples for all four 'this' keyword principles

--- assignments/this.js
+++ assignments/this.js
@@ -13,16 +13,17 @@
         For instance, in the function invocation "myObj.sayHello('Tico')", the 
         "this" keyword is referring to the "myObj".
 
-* 3. Explicit Binding
-
-        This type of binding requires manually setting (by the developer) the context
-        of the "this" keyword with JavaScript's '.call()' and '.apply' methods.
-
-* 4. New Binding
+* 3. New Binding
 
         This type of binding sets the context of the "this" keyword to the specific
-        instance of the object that has been created and returned by the constructor
+        instance of the object that has been created and is returned by the constructor
         function.
+
+* 4. Explicit Binding
+
+        This type of binding requires manually setting (by the developer) the context
+        of the "this" keyword with JavaScript's ".call()" and ".apply" methods.
+        
 *
 * write out a code example of each explanation above
 */
@@ -31,14 +32,62 @@
 
 // code example for Window Binding
 
+// const hooper = {
+//     name: 'Jake Thomas',
+//     shoes: 'Nike',
+//     buckets: function() {
+//         // With window binding, 'shoes' cannot be found
+//       console.log(`${this.name} loves to wear ${shoes} sneakers when hooping.`);
+//     }
+//   };
+  
+//   hooper.buckets();
+
 // Principle 2
 
 // code example for Implicit Binding
+
+const hooper = {
+    name: 'Jake Thomas',
+    shoes: 'Nike',
+    buckets: function() {
+        // With Implicit binding, 'name' and 'shoes' can be found because the 'this' keyword is referring to the 'hooper' object
+      console.log(`${this.name} loves to wear ${this.shoes} sneakers when hooping.`);
+    }
+  };
+  
+  hooper.buckets();
 
 // Principle 3
 
 // code example for New Binding
 
+function allStar(player) {
+    this.allStar = player;
+    this.team = "West-Conference All Stars";
+    this.announcement = function() {
+      console.log(`${this.allStar} has just been named one of 2019's ${this.team}!`);
+    };
+  }
+// The 'new' keyword binds the context of the 'this' keyword to the 'allStar' constructor function above
+const durant = new allStar('Kevin Durant');
+
+// console.log(durant);
+durant.announcement();
+
 // Principle 4
 
 // code example for Explicit Binding
+
+const james = {
+    name: 'LeBron James',
+};
+
+const skills = ['out-of-this-world athleticism', 'exceptional play-making abilities', 'sick handles' ];
+
+function announcer(skills1, skills2, skills3) {
+    console.log(`Ladies and Gentlemen! Let me tell you why ${this.name} will dominate
+    in today's basketball game! This guy has: ${skills1}, ${skills2}, and ${skills3}!`);
+}
+
+announcer.call(james, ...skills);
