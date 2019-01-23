ad53f19c276dffb02b52243bdaa057e115573ee9
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-01-23 16:01:30 -0500

    Defined each principle of the 'this' keyword in the this.js file

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,28 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding
+
+        This type of binding sets the context of the "this" keyword to the 'window,'
+        which is, essentially, the outer-most scoped object of JavaScript.
+
+* 2. Implicit Binding
+
+        This type of binding automatically sets the context of the "this" keyword to
+        whatever object directly precedes the dot in the invocation of a function.
+        For instance, in the function invocation "myObj.sayHello('Tico')", the 
+        "this" keyword is referring to the "myObj".
+
+* 3. Explicit Binding
+
+        This type of binding requires manually setting (by the developer) the context
+        of the "this" keyword with JavaScript's '.call()' and '.apply' methods.
+
+* 4. New Binding
+
+        This type of binding sets the context of the "this" keyword to the specific
+        instance of the object that has been created and returned by the constructor
+        function.
 *
 * write out a code example of each explanation above
 */
